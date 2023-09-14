e34122491246eabcab0c7cd7cb6c04e0491e042b
AndyE Gómez <andygomezb7@gmail.com>
2023-09-14 09:05:21 -0600

    404 not found

--- product.php
+++ product.php
@@ -11,7 +11,9 @@
 		// );
 	}
 
-	$inventarioBodega = $inventario->obtenerTotalRepuestosPorBodega(@$isMyBodega['bodega_id'], $pr)->fetch_assoc();
+	$inventarioBodega = $inventario->obtenerTotalRepuestosPorBodega(@$isMyBodega['bodega_id'], $pr);
+    if (@$inventarioBodega->num_rows) {
+        $inventarioBodega = $inventarioBodega->fetch_assoc();
 ?>
 <div class="container">
         	<div class="row mt-4 pt-5">
@@ -68,3 +70,16 @@
         </div>
 <script type="text/javascript" src="scripts/zoomproduct.js"></script>
 <link rel="stylesheet" type="text/css" href="styles/product.css">
+<?php } else { ?>
+    <div class="page-wrap d-flex flex-row align-items-center my-5 py-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12 text-center">
+                    <span class="display-1 d-block">404</span>
+                    <div class="mb-4 lead">The page you are looking for was not found.</div>
+                    <a href="https://www.totoprayogo.com/#" class="btn btn-link">Back to Home</a>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php } ?>
