18ccefcc5be9a237ce506f3edc065c793378ca65
AndyE Gómez <andygomezb7@gmail.com>
2023-10-10 04:54:20 -0600

    fix bankbook

--- root/ajax/get_data_table.php
+++ root/ajax/get_data_table.php
@@ -829,8 +829,8 @@ switch ($method) {
             im.id AS movimientoid', $sql_countable) . $search_ql . $order_ql . " LIMIT $start, $length");
 
         // Obtener el número total de registros sin filtro
-        $resultTotal = $db->query(str_replace('{select}', 'count(im.id) AS total, 
-                                                (CASE WHEN im.tipo = "venta" THEN "Ingresos" WHEN im.tipo = "compra" THEN "Egresos" END) AS TipoCuenta ', $sql_countable));
+        $resultTotal = $db->query(str_replace('{select}', 'count(im.id) AS total,
+                            (CASE WHEN im.tipo = "venta" THEN "Ingresos" WHEN im.tipo = "compra" THEN "Egresos" END) AS TipoCuenta ', $sql_countable . $order_ql));
         $rowTotal = $resultTotal->fetch_assoc();
         $totalRegistros = $rowTotal['total'];
 
