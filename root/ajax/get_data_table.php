e3d7affff3c7af838738ce3891741e113bd3713a
AndyE Gómez <andygomezb7@gmail.com>
2023-10-10 04:40:10 -0600

    fix bankbook

--- root/ajax/get_data_table.php
+++ root/ajax/get_data_table.php
@@ -808,13 +808,26 @@ switch ($method) {
         $start_date = $_GET['start'];
         $end_date = $_GET['end'];
         $order_position = array("im.id", "im.id", "im.id", "im.fecha", "Tipo_Movimiento", "Descripcion", "Cuenta_Contable_Banco", "debe", "haber");
-        $order_ql = ($order ? " ORDER BY ".$order_position[$order[0]['column']] . " " . $order[0]['dir'] : " ORDER BY im.id DESC");
+        $order_ql = " GROUP BY im.id" . ($order ? " ORDER BY ".$order_position[$order[0]['column']] . " " . $order[0]['dir'] : " ORDER BY im.id DESC");
         $search_ql = ($search ? " WHERE (im.fecha BETWEEN '".$start_date."' AND '".$end_date."') AND (im.tipo = 'compra' OR im.tipo = 'venta') AND Descripcion LIKE '%$search%' AND im.empresa_id = " . $_SESSION['empresa_id'] : " WHERE (im.fecha BETWEEN '".$start_date."' AND '".$end_date."') AND (im.tipo = 'compra' OR im.tipo = 'venta') AND im.empresa_id = " . $_SESSION['empresa_id']);
 
         // Ejecutar la consulta y obtener los datos de cuentas de banco
         $sql_countable = "
-            SELECT {select} FROM inventario_movimientos im LEFT JOIN cuenta_contable AS cc ON (cc.TipoCuenta = 'Ingresos' AND im.tipo = 'venta') OR (cc.TipoCuenta = 'Egresos' AND im.tipo = 'compra') LEFT JOIN Banco b ON b.cuenta_contable_defecto_id = cc.ID";
-        $cuentasContables = $db->query(str_replace('{select}', 'im.tipo AS Tipo_Movimiento, im.fecha AS Fecha_Movimiento, (CASE im.tipo WHEN "venta" THEN im.cantidad ELSE 0 END) AS debe, (CASE im.tipo WHEN "compra" THEN im.cantidad ELSE 0 END) AS haber, im.comentario AS Descripcion, b.id AS Banco_ID, b.nombre_cuenta AS Nombre_Banco, cc.NombreCuenta AS Cuenta_Contable_Banco, im.id AS movimientoid', $sql_countable) . $search_ql . $order_ql . " LIMIT $start, $length");
+            SELECT {select} 
+                    FROM inventario_movimientos im 
+                    LEFT JOIN cuenta_contable AS cc ON 
+                    (cc.TipoCuenta = 'Ingresos' AND im.tipo = 'venta') OR 
+                    (cc.TipoCuenta = 'Egresos' AND im.tipo = 'compra') 
+            LEFT JOIN Banco b ON b.cuenta_contable_defecto_id = cc.ID
+            LEFT JOIN pedido_detalles AS pd ON im.pedido_id = pd.id_pedido AND im.repuesto_id = pd.id_repuesto AND im.tipo = 'venta'
+            LEFT JOIN compras_articulos AS ca ON im.compra_id = ca.compra_id AND im.repuesto_id = ca.repuesto_id AND im.tipo = 'compra'";
+        $cuentasContables = $db->query(str_replace('{select}', 'im.tipo AS Tipo_Movimiento, 
+            im.fecha AS Fecha_Movimiento, 
+            COALESCE(SUM( CASE WHEN im.tipo = "venta" THEN pd.cantidad * pd.precio_unitario ELSE 0 END ), 0) AS debe,
+            COALESCE(SUM( CASE WHEN im.tipo = "compra" THEN ca.cantidad * ca.precio ELSE 0 END ), 0) AS haber,
+            im.comentario AS Descripcion, b.id AS Banco_ID, 
+            b.nombre_cuenta AS Nombre_Banco, cc.NombreCuenta AS Cuenta_Contable_Banco, 
+            im.id AS movimientoid', $sql_countable) . $search_ql . $order_ql . " LIMIT $start, $length");
 
         // Obtener el número total de registros sin filtro
         $resultTotal = $db->query(str_replace('{select}', 'count(im.id) AS total ', $sql_countable));
